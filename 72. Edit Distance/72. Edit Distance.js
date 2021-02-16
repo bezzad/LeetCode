bc8d6d05f4ebec07413da3bfaae5f8357ed31507
Behzad <behzad.khosravifar@gmail.com>
2021-02-17 00:33:18 +0330

    Completed problem 72

--- 72. Edit Distance/72. Edit Distance.js
+++ 72. Edit Distance/72. Edit Distance.js
@@ -4,29 +4,34 @@
  * @return {number}
  */
 var minDistance = function (word1, word2) {
-    let largWord = word1.length >= word2.length ? word1 : word2;
-    let smallWord = word1.length >= word2.length ? word2 : word1;
-    let distance = 0;
-    for (let i = 0; i < smallWord.length; i++) {
-        for (let j = 0; j < largWord.length; j++) {
-            
+    let matrix = [];
+
+    if (word1.length * word2.length == 0) {
+        return word1.length + word2.length;
+    }
+
+    for (let i = 0; i < word1.length + 1; i++) {
+        matrix.push(new Array(word2.length + 1).fill(0));
+        matrix[i][0] = i;
+    }
+
+    for (let j = 0; j < word2.length + 1; j++) {
+        matrix[0][j] = j;
+    }
+    
+    for (let i = 1; i <= word1.length; i++) {
+        for (let j = 1; j <= word2.length; j++) {
+            if (word1[i - 1] == word2[j - 1]) {
+                matrix[i][j] = Math.min(1 + matrix[i - 1][j], 1 + matrix[i][j - 1], matrix[i - 1][j - 1]);
+            }
+            else {
+                matrix[i][j] = 1 + Math.min(matrix[i - 1][j], matrix[i][j - 1], matrix[i - 1][j - 1]);
+            }
         }
     }
-    return distance;
+
+    return matrix[word1.length][word2.length];
 };
-
-function insert(word, index, char) {
-    return word.slice(0, index) + char + word.slice(index);
-}
-
-function replace(word, index, char) {
-    return word.slice(0, index) + char + word.slice(index + 1);
-}
-
-function remove(word, index) {
-    return word.slice(0, index) + word.slice(index + 1);
-}
-
 
 console.assert(minDistance("horse", "ros") == 3, "1");
 console.assert(minDistance("intention", "execution") == 5, "2");
